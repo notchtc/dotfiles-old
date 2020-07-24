b73951d73e22a7a1a4baf18f01f89c1f684c6c49
cha0t1c <notnotcha0t1c@protonmail.com>
2020-07-24 14:50:34 +0200

    remove elfeed

--- config/fish/bobthe.fish
+++ config/fish/bobthe.fish
@@ -1,6 +1,6 @@
 set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
-set -g theme_color_scheme gruvbox
+set -g theme_color_scheme terminal
 set -g theme_display_date no
 set -g theme_display_user yes
 set -g theme_display_hostname yes
