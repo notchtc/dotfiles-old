35f777db5f8728fb477d63a469c8a89968a64e76
Cha0t1c <notnotcha0t1c@protonmail.com>
2020-11-02 17:35:18 +0100

    muh cleaning up home directory

--- config/fish/README.md
+++ config/fish/README.md
@@ -20,10 +20,20 @@ My personal fish config that doesn't look too crazy and doesn't have thousands o
 - Adds $HOME/.local/bin to PATH
 - Vi keybindings
 - Disable fish greeting
-- XDG
-  - Sets XDG\_DATA\_HOME to $HOME/.local/share
-  - Sets XDG\_CONFIG\_HOME to $HOME/.config
-  - Sets XDG\_CACHE\_HOME to $HOME/.cache
+- Cleaning up the home directory
+  - XDG
+    - Sets XDG\_DATA\_HOME to $HOME/.local/share
+    - Sets XDG\_CONFIG\_HOME to $HOME/.config
+    - Sets XDG\_CACHE\_HOME to $HOME/.cache
+    - Sets XDG\_DESKTOP\_DIR to $HOME/stuff/desktop
+    - Sets XDG\_TEMPLATES\_DIR to $HOME/stuff/misc
+    - Sets XDG\_PUBLICSHARE\_DIR to $HOME/stuff/pshare
+    - Sets XDG\_DOCUMENTS\_DIR to $HOME/stuff/docs
+    - Sets XDG\_MUSIC\_DIR to $HOME/stuff/music
+    - Sets XDG\_PICTURES\_DIR to $HOME/stuff/images
+    - Sets XDG\_VIDEOS\_DIR to $HOME/stuff/videos
+  - Moves inputrc to $XDG\_CONFIG\_HOME/readline/input
+  - Makes lesshist not appear
 - Applications
   - Sets BROWSER to firefox[^1]
   - Sets EDITOR to nvim[^1]
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,21 +5,32 @@ if not functions -q fisher
 	fish -c fisher
 end
 
-# Exports
+## EXPORTS ##
 # XDG
-set XDG_DATA_HOME $HOME/.local/share
-set XDG_CONFIG_HOME $HOME/.config
-set XDG_CACHE_HOME $HOME/.cache
+set XDG_DATA_HOME "$HOME/.local/share"
+set XDG_CONFIG_HOME "$HOME/.config"
+set XDG_CACHE_HOME "$HOME/.cache"
+set XDG_DESKTOP_DIR "$HOME/stuff/desktop"
+set XDG_DOWNLOAD_DIR "$HOME/stuff/downloads"
+set XDG_TEMPLATES_DIR "$HOME/stuff/misc"
+set XDG_PUBLICSHARE_DIR "$HOME/stuff/pshare"
+set XDG_DOCUMENTS_DIR "$HOME/stuff/docs"
+set XDG_MUSIC_DIR "$HOME/stuff/music"
+set XDG_PICTURES_DIR "$HOME/stuff/images"
+set XDG_VIDEOS_DIR "$HOME/stuff/videos"
+# Cleaning up the home directory
+set INPUTRC "$XDG_CONFIG_HOME/readline/inputrc"
+set LESSHISTFILE -
 
 # Use these you filthy application
 set EDITOR "nvim"
 set BROWSER "firefox"
 
 # Other
-set -U SXHKD_SHELL sh
-set fish_user_paths $PATH $HOME/.local/bin
+set -U SXHKD_SHELL "sh"
+set fish_user_paths "$PATH" "$HOME/.local/bin"
 
-# Aliases
+## ALIASES ##
 # XBPS
 alias xi="sudo xbps-install -S"
 alias xup="sudo xbps-install -Su"
@@ -30,8 +41,9 @@ alias xrs="xbps-query -Rs"
 alias e="$EDITOR"
 alias se="sudo $EDITOR"
 
+## OTHER ##
 # Vi bindings baby
 fish_vi_key_bindings
 
-# Other
+# Disable fish greeting
 set -U fish_greeting
