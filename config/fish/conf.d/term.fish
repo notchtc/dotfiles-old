2373fe9891f689cc43d14b061f2297c2b746d105
Cha0t1c <notnotcha0t1c@protonmail.com>
2020-11-10 19:41:42 +0100

    fish terminal colorscheme

---
+++ config/fish/conf.d/term.fish
@@ -0,0 +1,26 @@
+# Fish Default Colorscheme with terminal colors
+set -g fish_color_normal normal
+set -g fish_color_command blue
+set -g fish_color_quote yellow
+set -g fish_color_redirection cyan
+set -g fish_color_end green
+set -g fish_color_error brred
+set -g fish_color_param cyan
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_search_match bryellow --background=brblack
+set -g fish_color_history_current --bold
+set -g fish_color_operator brcyan
+set -g fish_color_escape brcyan
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_valid_path --underline
+set -g fish_color_autosuggestion brblack
+set -g fish_color_user brgreen
+set -g fish_color_host normal
+set -g fish_color_cancel -r
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description yellow
+set -g fish_pager_color_prefix white --bold --underline
+set -g fish_pager_color_progress brwhite --background=cyan
+set -g fish_color_match --background=brblue
+set -g fish_color_comment red
