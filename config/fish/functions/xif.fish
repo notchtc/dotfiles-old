9ae65c89d3a7d44aa908404773a56b2ed10441c1
Cha0t1c <notnotcha0t1c@protonmail.com>
2020-12-11 19:50:36 +0100

    quick fix

--- config/fish/functions/xif.fish
+++ config/fish/functions/xif.fish
@@ -1,3 +1,3 @@
 function xif
-	xbps-query -Rs [a-Z] --regex | cut -d " " -f 2 | fzf -m --preview 'xbps-query -RS {1}' | xargs -ro sudo xbps-install -S
+	xbps-query -Rs [a-Z] --regex | cut -d " " -f 1,2 | fzf -m --preview 'xbps-query -RS {2}' | cut -d " " -f 2 | xargs -ro sudo xbps-install -S
 end
