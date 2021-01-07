69999bc4d4982f466a7224dc5344cbfae559811c
cha0t1c <notnotcha0t1c@protonmail.com>
2021-01-07 15:56:16 +0100

    man function now supports selecting multiple things

--- config/fish/functions/smn.fish
+++ config/fish/functions/smn.fish
@@ -1,4 +1,3 @@
 function smn -d "Search man pages"
-    apropos . | fzf --no-multi -q "$argv" | cut -d "(" -f 1 | xargs -ro man
+    apropos . | fzf --multi -q "$argv" | cut -d "(" -f 1 | xargs -ro man
 end
-
