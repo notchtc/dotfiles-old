229d893e034e4ee562acda2306aa86c6ece9862e
cha0t1c <notnotcha0t1c@protonmail.com>
2021-01-12 12:19:16 +0100

    forgot to change  to /remove symbolink link in setwal before setting wallpaper

--- config/fish/functions/yr.fish
+++ config/fish/functions/yr.fish
@@ -1,3 +1,3 @@
 function yr --description="View and remove installed packages with yay"
-    yay -Qq | fzf -q "$1" -m --preview 'yay -Qi {1}' | xargs -ro yay -Rns
+    yay -Qq | fzf -q "$argv" -m --preview 'yay -Qi {1}' | xargs -ro yay -Rns
 end
