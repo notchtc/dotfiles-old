0ea4b261e30b90eac3f5a28c5634b1066a163288
cha0t1c <notnotcha0t1c@protonmail.com>
2021-02-03 10:12:41 +0100

    fix multi selection in yr

--- config/fish/functions/yr.fish
+++ config/fish/functions/yr.fish
@@ -1,3 +1,3 @@
 function yr --description="View and remove installed packages with paru"
-    paru -Qq | fzf -q "$argv" -m --preview 'paru -Qi {1}' | xargs -ro paru -Rns
+    paru -Qq | fzf -q "$argv" -m --preview 'paru -Qi {1}' | tr "\n" " " | xargs -ro paru -Rns
 end
