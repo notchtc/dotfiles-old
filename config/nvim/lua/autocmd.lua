0478fc49cbe05972378158da471801aa30fc1ad0
Cha0t1c <notnotcha0t1c@protonmail.com>
2021-02-16 14:29:11 +0100

    the cursor is a beam/restore cursor after exiting vim

--- config/nvim/lua/autocmd.lua
+++ config/nvim/lua/autocmd.lua
@@ -1,3 +1,6 @@
+-- Fix cursor when exiting
+vim.cmd('autocmd VimLeave * set guicursor=a:ver50')
+
 -- Remove trailing whitespace and newlines when saving
 vim.cmd([[autocmd BufWritePre * %s/\s\+$//e]])
 vim.cmd([[autocmd BufWritePre * %s/\n\+\%$//e]])
