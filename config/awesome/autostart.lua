aa401851f88a8ea1632a5c50202d3f22c5748436
Cha0t1c <notnotcha0t1c@protonmail.com>
2021-02-24 10:18:12 +0100

    update file

--- config/awesome/autostart.lua
+++ config/awesome/autostart.lua
@@ -1,3 +1,7 @@
 local awful = require("awful")
 
+awful.spawn.once("xrdb -merge $HOME/.Xresources")
+awful.spawn.once("setxkbmap -option caps:swapescape")
+awful.spawn.once("pulseaudio --start")
+awful.spawn.once("[ ! -s ~/.config/mpd/pid ] && mpd")
 awful.spawn.once("mpDris2")
