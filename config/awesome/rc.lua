68fc6329db82731110847f534267750b2eedf173
Cha0t1c <notnotcha0t1c@protonmail.com>
2021-03-17 09:08:46 +0100

    awesome: add garbage collection

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -31,3 +31,8 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_color_active end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_color_normal end)
+
+-- GARBAGE COLLECTION
+collectgarbage("setpause", 110)
+collectgarbage("setstepmul", 1000)
+collectgarbage("step", 1024)
