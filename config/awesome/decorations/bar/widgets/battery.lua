137965ce130ef3e27d311d1027768d7b4d676fa3
Cha0t1c <notnotcha0t1c@protonmail.com>
2021-03-18 14:13:28 +0100

    awesome: change icon of battery and volume depending on state

--- config/awesome/decorations/bar/widgets/battery.lua
+++ config/awesome/decorations/bar/widgets/battery.lua
@@ -1,8 +1,25 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
 
+local command = [[
+sh -c '
+state=$(acpi | cut -d " " -f 3 | cut -d "," -f 1)
+percent=$(acpi | cut -d " " -f 4 | cut -d "," -f 1)
+
+if [ "$state" = "Full" ]; then
+    icon=
+elif [ "$state" = "Charging" ]; then
+    icon=
+elif [ "$state" = "Discharging" ]; then
+    icon=
+fi
+
+echo "$icon $percent"
+'
+]]
+
 -- Create battery widget
-mybat = awful.widget.watch('sh -c \'acpi | cut -d " " -f 3,4 | cut -d "," -f 1,2\'', 25, function(widget, stdout)
-    widget:set_markup("<span font=\"" .. beautiful.icon_font .. "\" color=\"" .. beautiful.bg_normal .. "\"> " .. stdout .. "</span>")
+mybat = awful.widget.watch(command, 25, function(widget, stdout)
+    widget:set_markup("<span font=\"" .. beautiful.icon_font .. "\" color=\"" .. beautiful.bg_normal .. "\">" .. stdout .. "</span>")
     collectgarbage("collect")
 end)
--- config/awesome/decorations/bar/widgets/volume.lua
+++ config/awesome/decorations/bar/widgets/volume.lua
@@ -1,8 +1,31 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
 
+local command = [[
+sh -c '
+percent=$(pamixer --get-volume)
+muted=$(pamixer --get-volume-human)
+
+if [ "$muted" = "muted" ]; then
+    icon=婢
+else
+    if [ "$percent" -ge 55 ]; then
+        icon=墳
+    elif [ "$percent" -gt 40 ]; then
+        icon=奔
+    elif [ "$percent" -gt 0 ]; then
+        icon=奄
+    elif [ "$percent" -eq 0 ]; then
+        icon=ﱝ
+    fi
+fi
+
+echo "$icon $percent%"
+'
+]]
+
 -- Create volume widget
-myvol, myvolTimer = awful.widget.watch('sh -c "pamixer --get-volume-human"', 17, function(widget, stdout)
-    widget:set_markup("<span font=\"" .. beautiful.icon_font .. "\" color=\"" .. beautiful.bg_normal .. "\">墳 " .. stdout .. "</span>")
+myvol, myvolTimer = awful.widget.watch(command, 17, function(widget, stdout)
+    widget:set_markup("<span font=\"" .. beautiful.icon_font .. "\" color=\"" .. beautiful.bg_normal .. "\">" .. stdout .. "</span>")
     collectgarbage("collect")
 end)
