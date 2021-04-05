38008d1f044e89163f33214311daa889e1790bfa
chtc <notnotcha0t1c@protonmail.com>
2021-04-05 15:06:16 +0200

    Revert "awesome: remove focused window from bar"
    This reverts commit 487b443b0c2f0ecf545834aaf32c4150195f0db6.

--- config/awesome/decorations/widgets/tasklist.lua
+++ config/awesome/decorations/widgets/tasklist.lua
@@ -20,6 +20,20 @@ local tasklist_buttons = gears.table.join(
 
 awful.screen.connect_for_each_screen(function(s)
 
+    s.myfocusedwindow = awful.widget.tasklist {
+        screen  = s,
+        filter  = awful.widget.tasklist.filter.focused,
+        buttons = tasklist_buttons,
+        widget_template = {
+            {
+                {id = 'text_role', widget = wibox.widget.textbox},
+                layout = wibox.layout.flex.horizontal
+            },
+            id = 'background_role',
+            widget = wibox.container.background
+        }
+    }
+
     s.myminimizedlist = awful.widget.tasklist {
         screen   = s,
         filter   = awful.widget.tasklist.filter.minimizedcurrenttags,
@@ -36,14 +50,18 @@ awful.screen.connect_for_each_screen(function(s)
         widget_template = {
             {
                 {
-                    id     = "text_role",
-                    widget = wibox.widget.textbox
+                    {
+                        id     = "text_role",
+                        widget = wibox.widget.textbox
+                    },
+                    layout = wibox.layout.fixed.horizontal
                 },
-                layout = wibox.layout.fixed.horizontal
+                left = 3,
+                right = 3,
+                widget = wibox.container.margin
             },
-            left = 3,
-            right = 3,
-            widget = wibox.container.margin
+            id     = "background_role",
+            widget = wibox.container.background
         },
     }
 end)
