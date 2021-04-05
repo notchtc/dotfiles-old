4fc40820377e53070434a46dd936d70f8896d93c
chtc <notnotcha0t1c@protonmail.com>
2021-04-05 14:03:41 +0200

    ncmpcpp: change visualizer to wave_filled
    awesome: change mpd autostart command

--- config/awesome/autostart.lua
+++ config/awesome/autostart.lua
@@ -11,6 +11,6 @@ end
 
 run_once("xrdb -merge $HOME/.Xresources")
 run_once("pulseaudio --start")
-run_once("[ ! -s " .. gears.filesystem.get_xdg_config_home() .. "mpd/pid ] && mpd")
+run_once("mpd")
 run_once("mpDris2")
 run_once("redshift-gtk")
