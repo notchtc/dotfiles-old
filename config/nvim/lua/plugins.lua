9290e68ff991aa61e7b10ca3a90414f5f27a664a
chtc <notnotcha0t1c@protonmail.com>
2021-07-11 15:14:15 +0200

    nvim: add () around 'packadd packer.nvim'

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -2,7 +2,7 @@ local install_path = vim.fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     vim.cmd('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-    vim.cmd'packadd packer.nvim'
+    vim.cmd('packadd packer.nvim')
 end
 
 vim.cmd('packadd packer.nvim')
