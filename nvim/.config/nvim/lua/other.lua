4f880b3f33aa64fe22f420fd9fdefcb003744f80
chtc <notnotcha0t1c@protonmail.com>
2021-09-03 17:04:35 +0200

    rofi+dunst: make shit square
    nvim: make autocmds work
    
    nvim: move .stylua.toml back to nvim config dir

--- nvim/.config/nvim/lua/other.lua
+++ nvim/.config/nvim/lua/other.lua
@@ -1,9 +1,8 @@
+local cmd = vim.cmd
+
 -- Remove trailing whitespaces and newlines when saving
 cmd [[ au BufWritePre * %s/\s\+$//e ]]
 cmd [[ au BufWritePre * %s/\n\+\%$//e ]]
-
--- Enable rasi highlighting
-cmd [[ au BufNewFile,BufRead /*.rasi setf css ]]
 
 -- We can write to the shada file now
 vim.opt.shadafile = ""
